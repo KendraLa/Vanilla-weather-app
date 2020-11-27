a89613ea7d040720f00c1f8c2c3efcf8eb96f810
Kendra <kendralambillon@gmail.com>
2020-11-27 11:35:08 +0100

    Delete comments

--- src/app.js
+++ src/app.js
@@ -70,41 +70,6 @@ search("New York");
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-//step 1: create the variables apiKey and apiUrl
-//step 2: call axios
-//step 3: create the function displayTemperature(response)
-//DISPLAY THE TEMPERATURE
-//step 4: search where is temperature inside my console.log --> main.temp
-//step 5: from html remove the number degree and add an id name
-//step 6: do the same for the city name
-//step 7: do the same for the weather condition
-//step 8: create the variable temperatureElement and select it
-//step 9: do the same to DISPLAY THE CITY
-//console.log(response.data) and search where is the city --> data.name
-//DISPLAY THE CONDITION
-//step 10: create and select the description
-//DISPLAY THE HUMIDITY
-//step 11: delete de 89% and create a span after Humidity and an an id to the span
-// step 12: create the new element for humidity
-//step 13: do the same for Wind
-// DISPLAY THE CURRENT DATE
-//step 14: add an id and delete Wednesday 09:00
-//step 15 : create the element and select it
-//step 16: create the function in order to display the date as Wednesday 09:00
-//step 17: create the variable date, hours, minutes, day
-//step 18: create an ary for all days of the week start on Sunday
-//step 19: change --> let day = date.getDay(); to --> let day = days[date.getDay()];
-//step 20: put the if condition for minutes and hours in order to display well the current hour
-//step 21: remove the console.log
-//step 22: create the variable city and put it on the apiUrl
-//DISPLAY the correct icon weather
-//step 23: add an id to img on html + create icon element, select it and setAttribute
-//step 24: remove the link of src img on html
-//step 25: To change the alternative text. Create a setAttribute using hte same technique.
-//Create my search engine
-//step 26: add form element on html + add an id + create the inputs of the form
-//step 27: creat a div.row in order to align them with 2 columns + add a class to my button in order to have it better.
-//step 28: in order to add some margin at the bottom, add a class to my form on html file
 //LET'S LINK THE FORM
 //step 29: create the from variable and select it + add an eventListener
 //step 30: create the function and add an id to my input
